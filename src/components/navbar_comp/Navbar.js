6bc8a3210f1afa87bd432b7dfc00b32c25bed52c
Willson Wang <40642117+wangwillson1@users.noreply.github.com>
2020-05-09 23:30:07 -0400

    Simplified navbar

--- src/components/navbar_comp/Navbar.js
+++ src/components/navbar_comp/Navbar.js
@@ -1,32 +1,14 @@
 import React from 'react';
 import './Navbar.css';
 import NavbarItem from './NavbarItem';
-import Logo from './Logo';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 class Navbar extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {navOpen: true};
-        this.toggleNav = this.toggleNav.bind(this);
-    }
-
-    toggleNav() {
-        this.setState((currentState) => ({
-            navOpen: !currentState.navOpen
-        }))
-    }
-
     render() {
         return (
             <div className="nav-container">
-                <Logo className="resp"/>
-                <FontAwesomeIcon icon="bars" className="resp" onClick={this.toggleNav} />
-                <NavbarItem content="About"/>
-                <NavbarItem content="Skills"/>
                 <NavbarItem content="Portfolio"/>
-                <NavbarItem content="Connect"/>
-                
+                <NavbarItem content="Resume"/>
+                <NavbarItem content="Contact"/>
             </div>
         )
     }
