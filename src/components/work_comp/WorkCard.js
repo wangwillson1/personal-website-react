2edd3441025f91ee056e95d0edd3a65b0d87d2b1
Willson Wang <40642117+wangwillson1@users.noreply.github.com>
2020-05-13 10:57:44 -0400

    css fix

--- src/components/work_comp/WorkCard.js
+++ src/components/work_comp/WorkCard.js
@@ -7,7 +7,7 @@ class WorkCard extends React.Component {
             <div className="work-card">
                 <h3 className="date">{this.props.date}</h3>
                 <h2>{this.props.posn}</h2>
-                <a href={this.props.url}><h3 className="underline">{this.props.company}</h3></a>
+                <a href={this.props.url} rel="noopener noreferrer" target="_blank"><h3 className="underline">{this.props.company}</h3></a>
                 <p>{this.props.summary}</p>
             </div>
         )
